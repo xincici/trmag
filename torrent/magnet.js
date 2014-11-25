3429edfc108fe8a5598239350c37bdbd9c6aad1d
linye <linye@meituan.com>
2014-11-25 16:56:03 +0800

    improve magnet address

--- torrent/magnet.js
+++ torrent/magnet.js
@@ -5,10 +5,9 @@ function magnet( word ){
     var url = "http://torrentproject.com/" + word +'/';
     console.log( 'fetching magnet address...\n' );
     req.get(url, function(err, res, body){
-        var reg = /magnet:(.*?)announce'/g;
+        var reg = /magnet:(.*?)(?=&amp;)/g;
         var arr = body.match(reg);
         var str = arr[0];
-        str = str.substr(0, str.length-1);
         console.log(str + '\n');
     });
 }
