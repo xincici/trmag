618f06c3486b354ec957cd90b43a9e4b3a569c19
linye <linye@meituan.com>
2014-12-05 11:52:55 +0800

    use better output

--- torrent/torrent.js
+++ torrent/torrent.js
@@ -1,5 +1,7 @@
 var req = require('request');
 var console = require('console');
+var Table = require("cli-table");
+var cliff = require("cliff");
 
 function torrent( word ){
     var url = "http://torrentproject.com/";
@@ -7,36 +9,63 @@ function torrent( word ){
         out : 'json',
         s : word 
     };
-    function serilize(obj){
-        var arr = [];
-        for(var key in obj){
-            arr.push( key + '=' + encodeURIComponent(obj[key]) );
-        }
-        return arr.join('&');
-    }
-
     var fullUrl = url + '?' + serilize(data);
     console.log( 'searching...\n' );
     req.get(fullUrl, function(err, res, body){
         var ret = JSON.parse(body);
         console.log('种子总数：' + ret.total_found);
         var i = '1';
+        var arr = [];
         while( ret[i] && parseInt(i) <= 10 ){
+            /*
             console.log( i + '==============================================' );
             console.log('  种子标题: ' + ret[i].title );
             console.log('  种子大小: ' + sizeBetter( ret[i].torrent_size ) );
             console.log('  种子hash: ' + ret[i].torrent_hash );
+            */
+            arr.push( ret[i] );
             i = (parseInt(i) + 1).toString();
         }
+        //showTable( arr );
+        showCliff( arr );
     });
-    function sizeBetter(size){
-        if(size > 1024 * 1024 * 1024){
-            return (size/1024/1024/1024).toFixed(2) + 'G';
-        }else if(size > 1024 * 1024){
-            return (size/1024/1024).toFixed(2) + 'M';
-        }else{
-            return (size/1024).toFixed(2) + 'K';
-        }
+}
+function serilize(obj){
+    var arr = [];
+    for(var key in obj){
+        arr.push( key + '=' + encodeURIComponent(obj[key]) );
     }
+    return arr.join('&');
+}
+function sizeBetter(size){
+    if(size > 1024 * 1024 * 1024){
+        return (size/1024/1024/1024).toFixed(2) + 'G';
+    }else if(size > 1024 * 1024){
+        return (size/1024/1024).toFixed(2) + 'M';
+    }else{
+        return (size/1024).toFixed(2) + 'K';
+    }
+}
+function showTable(arr){
+    var table = new Table({
+        head : ["hash", "size", "标题"]
+    });
+    arr.forEach(function(item){
+        table.push([
+            item.torrent_hash, sizeBetter( item.torrent_size ), item.title
+        ]);
+    });
+    console.log( table.toString() );
+}
+function showCliff(arr){
+    var rows = [
+        ["hash", "size", "标题"]
+    ];
+    arr.forEach(function(item){
+        rows.push([
+            item.torrent_hash, sizeBetter( item.torrent_size ), item.title
+        ]);
+    });
+    console.log(cliff.stringifyRows(rows, ['red', 'blue', 'green']));
 }
 exports.torrent = torrent;
