3eb24fb7481ed6183e0e92ba0635ba431e465a2f
linye <linye@meituan.com>
2016-03-21 11:43:47 +0800

    update

--- torrent.js
+++ torrent.js
@@ -5,7 +5,6 @@ var log4js = require('log4js'),
 var cliff = require("cliff");
 
 function torrent( program ){
-    //tryBrisk( program );
     tryTorrentProject( program );
     //tryYtsto( program );
 }
@@ -20,47 +19,25 @@ function tryTorrentProject( program ){
     logger.info( 'searching from torrentproject.se...' );
     req.get(fullUrl, function(err, res, body){
         if( err ){
-            logger.error('errors happen, maybe the network is invalid!\n');
+            logger.error('Errors happen, maybe the network is invalid!\n');
             return;
         }
-        var ret = JSON.parse(body);
-        logger.info('种子总数：' + ret.total_found);
-        var i = '1';
-        var arr = [];
-        while( ret[i] && parseInt(i) <= ( program.size || 10 ) ){
-            arr.push( ret[i] );
-            i = (parseInt(i) + 1).toString();
+        try{
+            var ret = JSON.parse(body);
+            logger.info('种子总数：' + ret.total_found);
+            var i = '1';
+            var arr = [];
+            while( ret[i] && parseInt(i) <= ( program.size || 10 ) ){
+                arr.push( ret[i] );
+                i = (parseInt(i) + 1).toString();
+            }
+            showCliffTorrentProject( arr );
+        }catch(e){
+            logger.error('Errors happen, service is not available now!\n');
         }
-        showCliffTorrentProject( arr );
     });
 }
 
-function tryBrisk( program ){
-    var url = 'http://brisk.eu.org/api/magnet.php';
-    var data = {
-        q : program.name || ''
-    };
-    var fullUrl = url + '?' + serilize(data);
-    logger.info( 'searching from brisk.eu.org...' );
-    var options = {
-        url: fullUrl,
-        headers: {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36',
-        'Referer': 'http://brisk.eu.org/'
-        }
-    };
-    req.get(options, function(err, res, body){
-        if( err ){
-            logger.error('errors happen again, maybe the network is invalid!\n');
-            return;
-        }
-        var ret = JSON.parse(body);
-        logger.info('种子总数：' + ret.length);
-        var i = 0;
-        var arr = ret.slice(0, program.size || 10);
-        showCliffBrisk( arr );
-    });
-}
 function tryYtsto(program){
     var url = 'https://yts.to/api/v2/list_movies.json';
     var data = {
@@ -87,7 +64,6 @@ function tryYtsto(program){
     });
 }
 
-
 function serilize(obj){
     var arr = [];
     for(var key in obj){
@@ -109,15 +85,6 @@ function showCliffTorrentProject(arr){
     arr.forEach(function(item){
         rows.push([
             'magnet:?xt=urn:btih:' + item.torrent_hash, sizeBetter( item.torrent_size ), item.title
-        ]);
-    });
-    console.log(cliff.stringifyRows(rows, ['red', 'blue', 'green']));
-}
-function showCliffBrisk(arr){
-    var rows = [ ["magnet", "size", "标题"] ];
-    arr.forEach(function(item){
-        rows.push([
-             item.magnet.split('&')[0], item.size, item.title
         ]);
     });
     console.log(cliff.stringifyRows(rows, ['red', 'blue', 'green']));
